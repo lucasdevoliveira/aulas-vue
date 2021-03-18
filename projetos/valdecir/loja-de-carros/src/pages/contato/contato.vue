6a7bca332ccc97f2cdcc98564905f732fd118a5d
picotti <44068139+picotti@users.noreply.github.com>
2021-03-18 12:51:26 -0300

    Add files via upload

---
+++ projetos/valdecir/loja-de-carros/src/pages/contato/contato.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Contato</h1>
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+
+
+<script>
+    export default {
+        name: 'Contato'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ projetos/valdecir/loja-de-carros/src/pages/home/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header />
+        <Banner />
+        <Cars />
+    </div>
+</template>
+<script>
+    estou aqui
+    import Header from '../../components/Header.vue'
+    import Banner from '../../components/Banner.vue'
+    import Cars from '../../components/Cars.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            Header,
+            Banner,
+            Cars
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
